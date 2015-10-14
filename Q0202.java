e2530aa10d3f47e9bc980902af273e07a59eae1b
s13008 <s13008@std.it-college.ac.jp>
2015-10-14 18:04:04 +0900

    修正

--- Q0202.java
+++ Q0202.java
@@ -1,13 +1,10 @@
 class Q0202 {
-
-	static 
-
 	public static void main(String []args) {
-	double a = 5.5;
-	double b = 10.0;
-	double c = 12.1;
-	
-	double x = a+b+c;
-	System.out.print(x/3);
+		double a = 5.5;
+		double b = 10.0;
+		double c = 12.1;
+
+		double x = a+b+c;
+		System.out.print(x/3);
 	}
 }
