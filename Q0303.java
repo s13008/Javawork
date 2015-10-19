e2a038a627d2fea389ebdabbfe6ff40cd6b65bf5
s13008 <s13008@std.it-college.ac.jp>
2015-10-19 17:10:57 +0900

    課題

---
+++ Q0303.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Q0303 {
+	public static void main(String []args) {
+
+		Scanner std = new Scanner(System.in);
+
+		boolean handler = false;
+		int rate = 0;
+
+		while(!handler){
+			System.out.print("\n" + "降水確率を入力してください：");
+			rate = std.nextInt();
+			if(rate < 0 || rate > 100){
+				System.out.println("\n" + "降水確率は0から100の間で指定してください。\n");
+			}else{
+				handler = true;
+			}
+		}
+
+		if(rate < 0 || rate < 19){
+			System.out.println("\n" + rate + "%: 傘は要りません。\n");
+		}else if(rate < 20 || rate < 49){
+			System.out.println("\n" + rate + "%: 雨が降るかもしれません。傘を持って行ったほうが良いでしょう。\n");
+		}else{
+			System.out.println("\n" + rate + "%: 傘を持って行ったほうが良いでしょう。\n");
+
+		}
+	}
+}
